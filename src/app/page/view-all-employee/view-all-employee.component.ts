9aad8eab3561d9321dea06e7817c0035bb7eeb9d
KasunChethiya <kcrathnamalala@gmail.com>
2024-07-25 15:50:37 +0530

    genarated view-all-employee component

---
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-all-employee.component.html',
+  styleUrl: './view-all-employee.component.css'
+})
+export class ViewAllEmployeeComponent {
+
+}
